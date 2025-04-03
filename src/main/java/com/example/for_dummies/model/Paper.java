7e785eaf71fdae6f3a037ebc724ec185c2167d49
AbirAbidi <abira7344@gmail.com>
2025-04-03 23:23:26 +0100

    new properties paper

--- src/main/java/com/example/for_dummies/model/Paper.java
+++ src/main/java/com/example/for_dummies/model/Paper.java
@@ -1,5 +1,26 @@
 package com.example.for_dummies.model;
 
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "papers")
 public class Paper {
 
+	@Id 
+	private String id;
+	private String title ;
+	private String authot_id;
+	private Date created_at;
+	private List<String> keywords;  
+	private int readabilityScore;  
+	private int viewCount; 
+	private int likesCount;  
+
+	//TODO add constrcutor of paper
+	//TODO add the files after ai logic
+
+	
 }
